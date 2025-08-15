53b901f2162e1f9922c25c3803660cede759bfc4
aman sharma <amanvilvas04@gmail.com>
2025-08-15 18:16:15 +0530

    finally image uploading working fine

--- server/controllers/user-conroller.js
+++ server/controllers/user-conroller.js
@@ -2,7 +2,7 @@
 const User = require('../models/user-model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const formidable = require('formidable')
+const {formidable} = require('formidable')
 const cloudinary = require('../config/cloudinary')
 
 exports.signin = async (req, res)=>{
@@ -226,71 +226,72 @@ exports.followUser = async (req, res)=>{
 }
 
 
-exports.updateProfile = async(req,res) =>{
-    try{
+exports.updateProfile = async (req, res) => {
+    try {
         const userExists = await User.findById(req.user._id)
-        if(!userExists){
+        if (!userExists) {
             return res.status(400).json({
                 msg: "user does not exists"
             })
         }
-        const form  = formidable({})
+        const form = formidable({ multiples: false });
 
         // fields-- media related text
         //files-- media filess
-            form.parse(req, async(err, fields, files)=>{
-                if(err){
-                    return res.status(400).json({
-                msg: "error in parsing the media files", err: err.message
-            })
-                }
-
-                if(fields.text){
-                    await User.findByIdAndUpdate(req.user._id, {
-                        bio: fields.text
-                    },
-                {
-                    new:true
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                return res.status(400).json({
+                    msg: "error in parsing the media files", err: err.message
                 })
-                }
-        //if pfp pehle se hi exists karti hai to delete the old one
-        if(files.media){
-             //if pfp pehle se hi exists karti hai to delete the old one
-            if(userExists.public_id){
-                await cloudinary.uploader.destroy(
-                    userExists.public_id,({error, result}) => {
-                        console.log({ error, result });
-                        
-                    }
-                )
-            
             }
-        const uploadedImage = await cloudinary.uploader.upload(files.media.filepath,
-            { folder: 'Threads_clone/Profiles' }
-        )
-        if(!uploadedImage){
-            return res.status(400).json({
-                msg:"file not uploaded successfully"
-            })
-        }
-        
-        await User.findByIdAndUpdate(
-            req.user._id,
-        { new: true }        )
 
-        }
+            if (fields.text) {
+                await User.findByIdAndUpdate(
+                    req.user._id,
+                    { bio: Array.isArray(fields.text) ? fields.text[0] : fields.text },
+                    { new: true }
+                )
+            }
 
-            })
+            //if pfp pehle se hi exists karti hai to delete the old one
+            if (files.media) {
+                // handle formidable array/object difference
+                let fileObj = Array.isArray(files.media) ? files.media[0] : files.media;
+                let filePath = fileObj.filepath || fileObj.path;
+
+                //if pfp pehle se hi exists karti hai to delete the old one
+                if (userExists.public_id) {
+                    await cloudinary.uploader.destroy(
+                        userExists.public_id, (error, result) => {
+                            console.log({ error, result });
+                        }
+                    )
+                }
+
+                const uploadedImage = await cloudinary.uploader.upload(filePath,
+                    { folder: 'Threads_clone/Profiles' }
+                )
+                if (!uploadedImage) {
+                    return res.status(400).json({
+                        msg: "file not uploaded successfully"
+                    })
+                }
+
+                await User.findByIdAndUpdate(
+                    req.user._id,
+                    { profilePic: uploadedImage.secure_url, public_id: uploadedImage.public_id },
+                    { new: true }
+                )
+            }
 
             res.status(201).json({
-               msg: 'pfp updated successfully'
+                msg: 'pfp updated successfully'
             })
+        })
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
             msg: "profile doesnt exists", err: err.message
         })
     }
 }
-
-//1:42
